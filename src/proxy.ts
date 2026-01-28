6ab1a93cd73a007f081dc3410b9f927183c6b6d3
Czar-16 <anoopjha2003@gmail.com>
2026-01-28 12:52:56 +0530

    added proxy/middleware

---
+++ src/proxy.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function proxy(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+
+  const isPublicPath =
+    path === "/login" || path === "/signup" || path === "/verifyemail";
+
+  const token = request.cookies.get("token")?.value || "";
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+}
+
+// Alternatively, you can use a default export:
+// export default function proxy(request: NextRequest) { ... }
+
+export const config = {
+  matcher: ["/", "/login", "/signup", "/profile", "/verifyemail"], // which page it should run 
+};
